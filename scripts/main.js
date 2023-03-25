eaabf4d3e2275e3d3ed7ce953931698cb176992c
smallpotato <smallpotato6035@gmail.com>
2023-03-25 17:35:27 +0100

    implement cart

--- scripts/main.js
+++ scripts/main.js
@@ -25,7 +25,8 @@ async function readFromJson(path) {
 function initButtons(){
     // cart
     document.getElementById('cart-btn').addEventListener('click', (e) => {
-        e.target.textContent = 'Clicked!';
+        e.target.textContent = 'Clicked!'
+        showCart()
       });
 
     // buy book
@@ -33,16 +34,46 @@ function initButtons(){
       buyBooks.forEach(buybtn => {
         buybtn.addEventListener('click', (e) => {
             console.log()
-            e.target.textContent = 'Clicked!';
+            e.target.textContent = 'Clicked!'
             addBookToCart(e.target.parentNode.id)
         });
       });
 }
 
 function addBookToCart(id){
-    let bookToAdd = books.find(element => element.id==id)
-    booksInCart.push(bookToAdd)
+    if (booksInCart.find(element => element.id==id)){
+        booksInCart.find(element => element.id==id).copiesInCart += 1
+    }
+    else {
+        let bookToAdd = books.find(element => element.id==id)
+        bookToAdd.copiesInCart = 1
+        booksInCart.push(bookToAdd)
+    }
     console.log(booksInCart)
+}
+
+function showDetails(id) {
+    return
+}
+
+function showCart(id) {
+    const htmlCartDiv = document.getElementById("js-cart-modal");
+    let htmlToRender = '';
+    let total = 0;
+    booksInCart.forEach((book) => {
+        htmlToRender += 
+        `<div class = 'cart container' id = '${book.id}'>
+            Title: ${book.title}<br>
+            Price: ${book.price}<br>
+            In cart: ${book.copiesInCart}
+            Subtotal: ${book.price*book.copiesInCart}
+        </div>`;
+        total += book.price*book.copiesInCart
+    })
+    
+    htmlToRender += `<div class = 'total container'>Total: ${total}</div>`
+    console.log(htmlCartDiv)
+    htmlCartDiv.innerHTML = htmlToRender;
 }
 
 // init
