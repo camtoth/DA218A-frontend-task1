977e541504803a8b37b9504e1cbb3db4406fb4f5
smallpotato <smallpotato6035@gmail.com>
2023-03-25 15:30:39 +0100

    add responsive cart button

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ function renderBooks() {
     let htmlToRender = '';
     books.forEach(function (book) {
         htmlToRender += 
-        `<div class = 'book' id = '${book.id}'>
+        `<div class = 'book container-sm' id = '${book.id}'>
             Title: ${book.title}<br>
             Author: ${book.author}
         </div>`;
@@ -20,11 +20,18 @@ async function readFromJson(path) {
     return await (await fetch(path)).json(); 
 }
 
+function initButtons(){
+    document.getElementById('cart-btn').addEventListener('click', (e) => {
+        e.target.textContent = 'Clicked!';
+      });
+}
+
 // init
 async function init() {
     books = await readFromJson('data/books.json');
 
     renderBooks()
+    //initButtons()
     //render all the menus and nav options
 }
 
