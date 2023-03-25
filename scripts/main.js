021dea1a9077b34bfe383b76eada82948013075e
smallpotato <smallpotato6035@gmail.com>
2023-03-25 16:12:18 +0100

    implement add books to cart functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
 //global variables
 let books;
+let booksInCart = [];
 
 // render functions
 function renderBooks() {
     const htmlBooksDiv = document.getElementById("js-books");
     let htmlToRender = '';
-    books.forEach(function (book) {
+    books.forEach((book) => {
         htmlToRender += 
         `<div class = 'book container-sm' id = '${book.id}'>
             Title: ${book.title}<br>
-            Author: ${book.author}
+            Author: ${book.author}<br>
+            <button type="button" class="btn btn-primary click" id="buy-btn">Buy ➕</button>
         </div>`;
     })
     htmlBooksDiv.innerHTML = htmlToRender;
@@ -21,9 +23,26 @@ async function readFromJson(path) {
 }
 
 function initButtons(){
+    // cart
     document.getElementById('cart-btn').addEventListener('click', (e) => {
         e.target.textContent = 'Clicked!';
       });
+
+    // buy book
+      const buyBooks = document.querySelectorAll('#buy-btn')
+      buyBooks.forEach(buybtn => {
+        buybtn.addEventListener('click', (e) => {
+            console.log()
+            e.target.textContent = 'Clicked!';
+            addBookToCart(e.target.parentNode.id)
+        });
+      });
+}
+
+function addBookToCart(id){
+    let bookToAdd = books.find(element => element.id==id)
+    booksInCart.push(bookToAdd)
+    console.log(booksInCart)
 }
 
 // init
@@ -31,7 +50,7 @@ async function init() {
     books = await readFromJson('data/books.json');
 
     renderBooks()
-    //initButtons()
+    initButtons()
     //render all the menus and nav options
 }
 
